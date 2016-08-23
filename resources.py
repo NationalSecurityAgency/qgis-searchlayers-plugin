af3bc2a1b782d4252397f4c26711177435cb199b
Hamilton <publisher@solarviews.com>
2016-08-23 11:50:31 -0400

    Machine generated

--- resources.py
+++ resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Fri Apr 15 15:53:02 2016
+# Created: Mon Aug 22 16:33:39 2016
 #      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
